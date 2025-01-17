bea0caa7ee90d1d0414043e2b93cdb04d406696d
cho <inbrain144@gmail.com>
2025-01-17 23:57:40 +0900

    2025.01.17 bfs k개의 벽 부수기
    - 백트래킹 + bfs 혼재해서 풀이
    - 틀린 부분이 좀 있어서 고치고자 함

---
+++ Codetree_Practice/breakWall.java
@@ -0,0 +1,125 @@
+import java.util.*;
+import java.io.*;
+
+class Point {
+    int r;
+    int c;
+
+    public Point(int r, int c){
+        this.r = r;
+        this.c = c;
+    }
+}
+
+public class breakWall {
+    static int n;
+    static int k;
+
+    static int[][] grid;
+    static boolean visited;
+    static int[][] step;
+    static Queue<Point> queue = new LinkedList<>();
+    static ArrayList<Point> list = new ArrayList<>();
+
+    static Point startPoint;
+    static Point endPoint;
+
+    static int answer = -1;
+
+    static int[] dirR = {-1,1,0,0};
+    static int[] dirC = {0,0,-1,1};
+
+    public static void findWall(){
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                if(grid[i][j] == 1){
+                    list.add(new Point(i, j));
+                }
+            }
+        }
+    }
+
+    public static void chooseWall(int wallNum, int cnt){
+        if(wallNum == list.size()+1){
+            if(cnt == k){
+                visited[startPoint.r][startPoint.c] = true;
+                queue.add(startPoint);
+                bfs();
+                init();
+            }
+            return 0;
+        }
+        Point currPoint = list[wallNum];
+
+        grid[currPoint.r][currPoint.c] = 0;
+        chooseWall(wallnum+1, cnt+1);
+        grid[currPoint.r][currPoint.c] = 1;
+
+        chooseWall(wallnum+1, cnt);
+    }
+
+    public static void bfs(){
+        while(!queue.isEmpty()){
+            Point currPoint = queue.poll();
+            int currNum = step[currPoint.r][currPoint.c];
+            if(currPoint.r == )
+
+                for(int i = 0; i<4; i++){
+                    int newR = currPoint.r+dirR[i];
+                    int newC = currPoint.c+dirC[i];
+
+                    if(canGo(newR, newC)){
+                        visited[newR][newC] = true;
+                        step[newR][newC] = currNum+1;
+                        queue.add(new Point(newR, newC));
+                    }
+                }
+        }
+    }
+
+    public static boolean canGo(int r, int c){
+        if(r<0 || r>=n || c<0 || c>=n) return false;
+        if(visited[r][c] || grid[r][c] == 1) return false;
+        return true;
+    }
+
+    public static void init(){
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                visited[i][j] = false;
+                step[i][j] = 0;
+            }
+        }
+        queue.clear();
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        grid = new int[n][n];
+        visited = new boolean[n][n];
+        step = new int[n][n];
+
+        for(int i = 0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j<n; j++){
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int r1 = Integer.parseInt(st.nextToken());
+        int c1 = Integer.parseInt(st.nextToken());
+        startPoint = new Point(r1, c1);
+
+        int r2 = Integer.parseInt(st.nextToken());
+        int c2 = Integer.parseInt(st.nextToken());
+        endPoint = new Point(r2, c2);
+    }
+}
+
+
